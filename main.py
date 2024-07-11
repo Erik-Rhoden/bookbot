834c443b2192dd499fa67fa83513e6b8c5bee624
Erik-Rhoden <erikrhoden@gmail.com>
2024-07-11 12:07:41 -0400

    add convert_dict and sort_on function; finished report in main(); update README.MD with description

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 def main():
-    with open("books/frankenstein.txt") as f:
+    book = "books/frankenstein.txt"
+    with open(book) as f:
         file_contents = f.read()
     word_count = count_words(file_contents)
     char_count = count_char(file_contents)
-    print(char_count)
+    dict_list = convert_dict(char_count)
+    dict_list.sort(reverse=True, key=sort_on)
+    print(f"--- Begin report of {book} ---")
+    print(f"{word_count} words found in the document\n")
+    for dict in dict_list:
+        letter, count = dict['letter'], dict['count']
+        print(f"The '{letter}' character was found '{count}' times")
+    print(f"--- End report ---")
+
 
 def count_words(file_contents):
     count = 0
@@ -20,5 +29,15 @@ def count_char(file_contents):
             char_dict[char] = 0
         char_dict[char] += 1
     return char_dict
+
+def convert_dict(char_count):
+    dict_list = []
+    for key, value in char_count.items():
+        if key.isalpha():
+            dict_list.append({'letter': key, 'count': value})
+    return dict_list
+
+def sort_on(dict_list):
+    return dict_list["count"]
 
 main()
